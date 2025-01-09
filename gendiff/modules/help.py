67ca96fa793e3435c99a6bcb123bb12d60e53f78
karanice <to.anathema@gmail.com>
2025-01-09 21:58:40 +0300

    moving parser (named help) to modules

--- gendiff/modules/help.py
+++ gendiff/modules/help.py
@@ -1,10 +1,14 @@
 import argparse
+from gendiff.modules.funcs import generate_diff
 
 parser = argparse.ArgumentParser(
     description='Compares two configuration files and shows a difference.')
 
-parser.add_argument('first_file')
-parser.add_argument('second_file')  
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', help='set format of output')
+args = parser.parse_args()
 
-def parser_print_help():
-    parser.print_help()
+
+def print_parser():
+    print(generate_diff(args.first_file, args.second_file))
