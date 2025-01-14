1c2c243dd2f5f0aff82dc82f126a0cc68be23781
karanice <to.anathema@gmail.com>
2025-01-14 20:05:27 +0300

    add tests

---
+++ tests/test_gendiff.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from gendiff.generate_diff import generate_diff
+from gendiff.parse_json import get_json
+
+generate_diff_plain_json = '{'\
+'  - follow: false'\
+'    host: hexlet.io'\
+'  - proxy: 123.234.53.22'\
+'  - timeout: 50'\
+'  + timeout: 20'\
+'  + verbose: true'\
+'}'
+
+
+def test_generate_diff_plain_json():
+    assert generate_diff('test_data/file1.json', 'test_data/file2.json') \
+        == generate_diff_plain_json
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_generate_diff_plain_json():
+    json1 = get_test_data_path('file1.json')
+    json2 = get_test_data_path('file2.json')
+    # parsed_json1 = get_json(json1)
+    # parsed_json2 = get_json(json2)
+    expected = read_file('plain_json_diff.txt')
+    actual = generate_diff(json1, json2)
+
+    assert actual == expected
