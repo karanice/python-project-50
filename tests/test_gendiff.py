4c113b38e76b98be223b3a1f13edbffedf99ca63
karanice <to.anathema@gmail.com>
2025-02-02 16:18:47 +0300

    add test data and tests for tree json

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,24 +1,11 @@
 from pathlib import Path
 from gendiff.generate_diff import generate_diff
-#from gendiff.parse_json import get_json
-
-generate_diff_plain_json = '{'\
-'  - follow: false'\
-'    host: hexlet.io'\
-'  - proxy: 123.234.53.22'\
-'  - timeout: 50'\
-'  + timeout: 20'\
-'  + verbose: true'\
-'}'
-
-
-def test_generate_diff_plain_json():
-    assert generate_diff('test_data/file1.json', 'test_data/file2.json') \
-        == generate_diff_plain_json
+from gendiff.formatter import default_formatter
 
 
 def get_test_data_path(filename):
     return Path(__file__).parent / 'test_data' / filename
+
 
 def read_file(filename):
     return get_test_data_path(filename).read_text()
@@ -27,10 +14,8 @@ def read_file(filename):
 def test_generate_diff_plain_json():
     json1 = get_test_data_path('file1.json')
     json2 = get_test_data_path('file2.json')
-    # parsed_json1 = get_json(json1)
-    # parsed_json2 = get_json(json2)
     expected = read_file('plain_json_diff.txt')
-    actual = generate_diff(json1, json2)
+    actual = default_formatter(generate_diff(json1, json2))
 
     assert actual == expected
 
@@ -39,6 +24,15 @@ def test_generate_diff_plain_yaml():
     yaml1 = get_test_data_path('file1.yml')
     yaml2 = get_test_data_path('file2.yml')
     expected = read_file('plain_yaml_diff.txt')
-    actual = generate_diff(yaml1, yaml2)
+    actual = default_formatter(generate_diff(yaml1, yaml2))
+
+    assert actual == expected
+
+
+def test_generate_diff_tree_json():
+    tree_json1 = get_test_data_path('file1_tree.json')
+    tree_json2 = get_test_data_path('file2_tree.json')
+    expected = read_file('tree_json_diff.txt')
+    actual = default_formatter(generate_diff(tree_json1, tree_json2))
 
     assert actual == expected
