8a787d75783912d1491d88965503f4517a226777
karanice <to.anathema@gmail.com>
2025-02-20 20:03:39 +0300

    update logic of generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,8 +1,7 @@
-from gendiff.parser import parse
 from gendiff.make_diff_tree import make_diff_tree
-from gendiff.styles.stylish import stylish
+from gendiff.parser import parse
 
 
-def generate_diff(file1_path, file2_path): # сюда должен добавляться форматтер??
+def generate_diff(file1_path, file2_path):
     file1, file2 = parse(file1_path), parse(file2_path)
     return make_diff_tree(file1, file2)
