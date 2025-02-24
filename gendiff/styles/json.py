129e83a7daf0a8e07ac69769ac27d6a00c3f0723
karanice <to.anathema@gmail.com>
2025-02-24 15:14:49 +0300

    add new output formats

---
+++ gendiff/styles/json.py
@@ -0,0 +1,5 @@
+import json as jsn
+
+
+def json(diff):
+    return jsn.dumps(diff, indent=4)
--- gendiff/styles/plain.py
+++ gendiff/styles/plain.py
@@ -1,14 +1,14 @@
 from itertools import chain
 
 
-def fix_key(key_to_fix):
+def fix_key(key_to_fix):  # убирает у ключей скобки с пояснениями
     return key_to_fix.split('(')[0]
 
 
-def fix_value(value):
-    if type(value) is dict:
+def fix_value(value):  # приведение значений в соответствие с примером
+    if isinstance(value, dict): 
         addition = '[complex value]'
-    elif type(value) is bool:
+    elif isinstance(value, bool):
         addition = str(value).lower()
     elif value is None:
         addition = 'null'
@@ -21,19 +21,19 @@ def plain(diff, prfx=''):
     res_str = []
     keys = list(chain(diff))
     fixed_keys = [fix_key(key) for key in keys]
+
     for key in keys:
+
         if fixed_keys.count(fix_key(key)) < 2:
             if '(added)' in key:
                 res_str.append(f'Property \'{prfx}{fix_key(key)}\' '
                                f'was added with value: {fix_value(diff[key])}')
             elif '(removed)' in key:
                 res_str.append(f'Property \'{prfx}{fix_key(key)}\' was removed')
-            else: 
-                if type(diff[key]) is dict:
-                    new_prfx = prfx + f'{fix_key(key)}.'
-                    res_str.append(plain(diff[key], new_prfx))
-                else:
-                    continue
+            elif isinstance(diff[key], dict):
+                new_prfx = prfx + f'{fix_key(key)}.'
+                res_str.append(plain(diff[key], new_prfx))
+
         elif type(diff[key] is not dict):
             added = f'{fix_key(key)}(added)'
             rmvd = f'{fix_key(key)}(removed)'
