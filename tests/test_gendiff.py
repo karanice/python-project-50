8d84642ba230035889a3c354e546141a6077c1db
karanice <to.anathema@gmail.com>
2025-03-21 13:58:42 +0300

    fix tests in accordance with changes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from gendiff.generate_diff import generate_diff
-from gendiff.styles.stylish import stylish
-from gendiff.styles.plain import plain
 
 
 def get_test_data_path(filename):
@@ -16,7 +14,7 @@ def test_generate_diff_plain_json():
     json1 = get_test_data_path('file1.json')
     json2 = get_test_data_path('file2.json')
     expected = read_file('plain_diff_stylish.txt')
-    actual = stylish(generate_diff(json1, json2))
+    actual = generate_diff(json1, json2, 'stylish')
 
     assert actual == expected
 
@@ -25,7 +23,7 @@ def test_generate_diff_plain_yaml():
     yaml1 = get_test_data_path('file1.yml')
     yaml2 = get_test_data_path('file2.yml')
     expected = read_file('plain_diff_stylish.txt')
-    actual = stylish(generate_diff(yaml1, yaml2))
+    actual = generate_diff(yaml1, yaml2, 'stylish')
 
     assert actual == expected
 
@@ -34,7 +32,7 @@ def test_generate_diff_tree_json():
     tree_json1 = get_test_data_path('file1_tree.json')
     tree_json2 = get_test_data_path('file2_tree.json')
     expected = read_file('tree_diff_stylish.txt')
-    actual = stylish(generate_diff(tree_json1, tree_json2))
+    actual = generate_diff(tree_json1, tree_json2, 'stylish')
 
     assert actual == expected
 
@@ -43,7 +41,7 @@ def test_generate_diff_tree_yaml():
     tree_yaml1 = get_test_data_path('file1_tree.yml')
     tree_yaml2 = get_test_data_path('file2_tree.yml')
     expected = read_file('tree_diff_stylish.txt')
-    actual = stylish(generate_diff(tree_yaml1, tree_yaml2))
+    actual = generate_diff(tree_yaml1, tree_yaml2, 'stylish')
 
     assert actual == expected
 
@@ -52,6 +50,6 @@ def test_generate_diff_plain_tree_json():
     tree_json1 = get_test_data_path('file1_tree.json')
     tree_json2 = get_test_data_path('file2_tree.json')
     expected = read_file('tree_diff_plain.txt')
-    actual = plain(generate_diff(tree_json1, tree_json2))
+    actual = generate_diff(tree_json1, tree_json2, 'plain')
 
     assert actual == expected
