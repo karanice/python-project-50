d6373fe087cc84516f1668576d1a0f1acc797628
karanice <to.anathema@gmail.com>
2025-03-21 13:56:50 +0300

    move formatter choice func to styles folder

---
+++ gendiff/styles/__init__.py
@@ -0,0 +1,16 @@
+from gendiff.styles.json import json
+from gendiff.styles.plain import plain
+from gendiff.styles.stylish import stylish
+
+def format_diff(diff, format):
+    match format:
+        case 'stylish':
+            return stylish(diff)
+        case 'plain':
+            return plain(diff)
+        case 'json':
+            return json(diff)
+        
+__all__ = (
+    format_diff
+)
