1943b6ca207609c1604563884eb8a7a5873c0a1a
karanice <to.anathema@gmail.com>
2025-04-04 16:09:14 +0300

    make parser a function

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,12 +1,13 @@
 import argparse
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.')
-
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-parser.add_argument('-f', '--format', default='stylish', help='set format of output')
-
 
 def get_args():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', default='stylish', 
+                        help='set format of output')
+
     return parser.parse_args()
