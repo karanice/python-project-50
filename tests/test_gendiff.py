b0cc8d8207eae96713946f9fff3d79ccd205fb7a
karanice <to.anathema@gmail.com>
2025-04-08 18:58:39 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -15,7 +15,7 @@ def test_to_stylish_plain_json():
     json1 = get_test_data_path('file1.json')
     json2 = get_test_data_path('file2.json')
     expected = read_file('plain_diff_stylish.txt')
-    actual = format_diff(generate_diff(json1, json2), 'stylish')
+    actual = generate_diff(json1, json2)
 
     assert actual == expected
 
@@ -24,7 +24,7 @@ def test_to_stylish_plain_yaml():
     yaml1 = get_test_data_path('file1.yml')
     yaml2 = get_test_data_path('file2.yml')
     expected = read_file('plain_diff_stylish.txt')
-    actual = format_diff(generate_diff(yaml1, yaml2), 'stylish')
+    actual = generate_diff(yaml1, yaml2)
 
     assert actual == expected
 
@@ -33,7 +33,7 @@ def test_to_stylish_tree_json():
     tree_json1 = get_test_data_path('file1_tree.json')
     tree_json2 = get_test_data_path('file2_tree.json')
     expected = read_file('tree_diff_stylish.txt')
-    actual = format_diff(generate_diff(tree_json1, tree_json2), 'stylish')
+    actual = generate_diff(tree_json1, tree_json2)
 
     assert actual == expected
 
@@ -42,7 +42,7 @@ def test_to_stylish_tree_yaml():
     tree_yaml1 = get_test_data_path('file1_tree.yml')
     tree_yaml2 = get_test_data_path('file2_tree.yml')
     expected = read_file('tree_diff_stylish.txt')
-    actual = format_diff(generate_diff(tree_yaml1, tree_yaml2), 'stylish')
+    actual = generate_diff(tree_yaml1, tree_yaml2)
 
     assert actual == expected
 
@@ -51,7 +51,7 @@ def test_to_plain_tree_json():
     tree_json1 = get_test_data_path('file1_tree.json')
     tree_json2 = get_test_data_path('file2_tree.json')
     expected = read_file('tree_diff_plain.txt')
-    actual = format_diff(generate_diff(tree_json1, tree_json2), 'plain')
+    actual = generate_diff(tree_json1, tree_json2, 'plain')
 
     assert actual == expected
 
