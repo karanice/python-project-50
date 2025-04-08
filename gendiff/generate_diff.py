b2e033dca971a2e50889c941950c41f6d2e68181
karanice <to.anathema@gmail.com>
2025-04-08 18:58:27 +0300

    add arg for choosing formatting

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,13 +2,14 @@ from os.path import splitext
 
 from gendiff.make_diff_tree import make_diff_tree
 from gendiff.parser import parse
+from gendiff.styles import format_diff
 
 
 def get_ext(file_path):
     return splitext(file_path)[1]
 
 
-def generate_diff(file1_path, file2_path):
+def generate_diff(file1_path, file2_path, format='stylish'):
     ext1, ext2 = get_ext(file1_path), get_ext(file2_path)
     file1, file2 = parse(file1_path, ext1), parse(file2_path, ext2)
-    return make_diff_tree(file1, file2)
+    return format_diff(make_diff_tree(file1, file2), format)
